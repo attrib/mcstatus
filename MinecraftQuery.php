f5438503a243b2ae4b1c9d6b8873db2c2f757c48
kfritsche <karl.fritsche@cocomore.com>
2012-08-17 21:33:49 +0200

    MinecraftQuery and StatusParser

--- MinecraftQuery.php
+++ MinecraftQuery.php
@@ -1,11 +1,202 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: kfritsche
- * Date: 8/17/12
- * Time: 3:10 PM
- * To change this template use File | Settings | File Templates.
- */
+
 class MinecraftQuery {
+
+  public static $MAGIC_PREFIX = "\xFE\xFD";
+  const PACKET_TYPE_CHALLENGE = 9;
+  const PACKET_TYPE_QUERY = 0;
+  public static $HUMAN_READABLE_NAMES = array(
+      'game_id' => "Game Name",
+      'gametype' => "Game Type",
+      'motd' => "Message of the Day",
+      'hostname' => "Server Address",
+      'hostport' => "Server Port",
+      'map' => "Main World Name",
+      'maxplayers' => "Maximum Players",
+      'numplayers' => "Players Online",
+      'players' => "List of Players",
+      'plugins' => "List of Plugins",
+      'raw_plugins' => "Raw Plugin Info",
+      'software' => "Server Software",
+      'version' => "Game Version",
+    );
+
+  private $host;
+  private $port;
+  private $timeout;
+  private $id;
+  private $retries = 0;
+  private $max_retries;
+  private $id_packed;
+  private $challenge;
+  private $challenge_packed;
+  private $socket;
+
+  public function __construct($host, $port, $timeout=10, $id=0, $retries=2) {
+    $this->host = gethostbyname($host);
+    $this->port = $port;
+    $this->timeout = array('sec' => $timeout, 'usec' => 0);
+    $this->id = $id;
+    $this->id_packed = pack('N', $id);
+    $this->challenge_packed = pack('N', 0);
+    $this->max_retries = $retries;
+
+    $this->socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+    socket_set_option($this->socket, SOL_SOCKET, SO_RCVTIMEO, $this->timeout);
+    socket_set_option($this->socket, SOL_SOCKET, SO_SNDTIMEO, $this->timeout);
+  }
+
+  private function sendRaw($data) {
+    $data = self::$MAGIC_PREFIX . $data;
+    $send = socket_sendto($this->socket, $data, strlen($data), 0, $this->host, $this->port);
+  }
+
+  private function sendPacket($type, $data='') {
+    $this->sendRaw(pack('C', $type) . $this->id_packed . $this->challenge_packed . $data);
+  }
+
+  private function readPacket() {
+    $len = socket_recvfrom($this->socket, $buff, 1460, 0, ip2long($this->host), $this->port);
+    if (!isset($len) || $len === FALSE) {
+      throw new Exception();
+    }
+    $type = array_shift(unpack('C', substr($buff, 0, 1)));
+    $id = array_shift(unpack('N', substr($buff, 1, 4)));
+    return array($type, $id, substr($buff, 5));
+  }
+
+  private function handshake($bypass_retries=FALSE) {
+    $this->sendPacket(self::PACKET_TYPE_CHALLENGE);
+
+    unset($this->challenge);
+
+    try {
+      list($type, $id, $buff) = $this->readPacket();
+    }
+    catch(Exception $e) {
+      if (!$bypass_retries) $this->retries++;
+      if ($this->retries < $this->max_retries) {
+        $this->handshake($bypass_retries);
+      }
+      return;
+    }
+
+    $this->challenge = (int) $buff;
+    $this->challenge_packed = pack('N', $buff);
+  }
+
+  public function getStatus() {
+    if (empty($this->challenge)) {
+      $this->handshake();
+    }
+
+    $this->sendPacket(self::PACKET_TYPE_QUERY);
+
+    try {
+      list($type, $id, $buff) = $this->readPacket();
+    }
+    catch(Exception $e) {
+      if ($this->retries < $this->max_retries) {
+        $this->handshake();
+        return $this->getStatus();
+      }
+      else {
+        return NULL;
+      }
+    }
+
+    $data = array();
+
+    $buff = explode("\x00", $buff, 6);
+    $data['motd'] = array_shift($buff);
+    $data['gametype'] = array_shift($buff);
+    $data['map'] = array_shift($buff);
+    $data['numplayers'] = (int) array_shift($buff);
+    $data['maxplayers'] = (int) array_shift($buff);
+    $buff = $buff[0];
+    $data['hostport'] = array_shift(unpack('v', substr($buff, 0, 2)));
+    $data['hostname'] = substr($buff, 2);
+
+    return $data;
+  }
+
+  public function getRules() {
+    if (empty($this->challenge)) {
+      $this->handshake();
+    }
+
+    $this->sendPacket(self::PACKET_TYPE_QUERY, $this->id_packed);
+
+    try {
+      list($type, $id, $buff) = $this->readPacket();
+    }
+    catch(Exception $e) {
+      if ($this->retries < $this->max_retries) {
+        $this->handshake();
+        return $this->getStatus();
+      }
+      else {
+        return NULL;
+      }
+    }
+
+    $data = array();
+
+    $buff = substr($buff, 11);
+    list($items, $players) = explode("\x00\x00\x01player_\x00\x00", $buff);
+
+    if (strpos($items, 'hostname') === 0) {
+      $items = 'motd'.substr($items, 8);
+    }
+
+    $items = explode("\x00", $items);
+    foreach ($items as $value) {
+      if (!isset($key)) {
+        $key = $value;
+      }
+      else {
+        if (in_array($key, array('numplayers', 'maxplayers', 'hostport'))) {
+          $data[$key] = (int) $value;
+        }
+        else {
+          $data[$key] = $value;
+        }
+        unset($key);
+      }
+    }
+
+    $players = substr($players, 0, -2);
+
+    if (!empty($players)) {
+      $data['players'] = explode("\x00", $players);
+    }
+    else {
+      $data['players'] = array();
+    }
+
+    if (!empty($data['plugins'])) {
+      $data['plugins_raw'] = $data['plugins'];
+      list($data['software'], $data['plugins']) = $this->parsePlugins($data['plugins']);
+    }
+
+    return $data;
+  }
+
+  private function parsePlugins($raw) {
+    $parts = explode(':', $raw);
+    $server = trim($parts[0]);
+    $plugins = array();
+
+    if (count($parts) == 2) {
+      $tmp_plugins = explode(';', $parts[1]);
+      $tmp_plugins = array_map('trim', $tmp_plugins);
+      foreach ($tmp_plugins as $value) {
+        $p = explode(' ', $value, 2);
+        $plugins[] = array('name' => $p[0], 'version' => $p[1]);
+      }
+    }
+
+    return array($server, $plugins);
+  }
 
 }
--- test.php
+++ test.php
@@ -1,8 +1,185 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: kfritsche
- * Date: 8/17/12
- * Time: 4:29 PM
- * To change this template use File | Settings | File Templates.
- */
+
+require 'MinecraftQuery.php';
+
+function getCurrentVersion($plugin_version, $versions) {
+  foreach ($versions as $version) {
+    if (preg_match('/(\d+\.\d+(\.\d+)?(-\S*)?(_\S*)?)/', $version['file_name'], $match)) {
+      if ($match[1] == $plugin_version) {
+        return $version;
+      }
+    }
+    elseif (strpos($plugin_version, $version['file_name']) !== false || strpos($version['file_name'], $plugin_version) !== false) {
+      return $version;
+    }
+  }
+  return NULL;
+}
+
+function getLastVersion($software, $versions) {
+  preg_match('/((\d+\.\d+\.\d+)-R\d\.\d)/', $software, $match);
+  $cb_version = 'CB '.trim($match[1]);
+  $mc_version = trim($match[2]);
+  for ($i=count($versions)-1; $i>0; $i--) {
+    $version = $versions[$i];
+    $version['bukkit_version'] = trim($version['bukkit_version']);
+    if ($cb_version == $version['bukkit_version'] || $mc_version == $version['bukkit_version']) {
+      return $version;
+    }
+  }
+  return false;
+}
+
+$query = new MinecraftQuery('mc.funky-clan.de', 25565);
+
+$rules = $query->getRules();
+
+// connect
+$m = new Mongo();
+// select a database
+$db = $m->bukkit;
+
+// select a collection (analogous to a relational database's table)
+$server_mods = $db->server_mods;
+
+$plugin_info = $server_mods->findOne(array('name' => 'CraftBukkit'));
+
+if (empty($plugin_info) || empty($plugin_info['versions']) || ($plugin_info['last_update']+86400 < time())) {
+  if (empty($plugin_info)) {
+    $plugin_info = array(
+      'name' => 'CraftBukkit',
+      'url' => 'http://dl.bukkit.org/downloads/craftbukkit/view/latest-rb/',
+      'last_update' => time(),
+      'versions' => array(),
+    );
+  }
+  elseif (!empty($plugin_info['versions'])) {
+    $added_versions = array();
+    foreach ($plugin_info['versions'] as $version) {
+      $added_versions[] = $version['file_name'];
+    }
+  }
+
+  if (!empty($plugin_info['url'])) {
+    echo "Updating Version History of CraftBukkit...";
+    $html = file_get_contents($plugin_info['url']);
+    if (preg_match('/<dt>\s*Upstream Artifacts:\s*<\/dt>\s*<dd>\s*<ul>(.*?)<\/ul>\s*<\/dd>/s', $html, $match)) {
+      if (preg_match('/<li>.*: <a href="(.*)">((.*)\(Build(.*)\))<\/a><\/li>/m', $match[1], $available_file)) {
+        $version = array(
+          'type' => 'R', //R, B, A
+          'url'  => 'http://dl.bukkit.org'.trim($available_file[1]),
+          'file_name' => trim($available_file[2]),
+          'bukkit_version' => 'CB '. trim($available_file[3]),
+          'bukkit_build' => trim($available_file[4]),
+          'time' => time(),
+        );
+        if (isset($added_versions) && !in_array($version['file_name'], $added_versions) || !isset($added_versions)) {
+          $plugin_info['versions'][] = $version;
+        }
+      }
+    }
+    $server_mods->save($plugin_info);
+    echo "done\n";
+  }
+}
+
+foreach($rules['plugins'] as $plugin) {
+  $plugin_info = $server_mods->findOne(array('name' => $plugin['name']));
+
+  if (!empty($plugin_info) && !empty($plugin_info['subModuleOf'])) {
+    $plugin_info = $server_mods->findOne(array('name' => $plugin_info['subModuleOf']));
+  }
+
+
+  if (empty($plugin_info) || empty($plugin_info['versions']) || ($plugin_info['last_update']+86400 < time())) {
+    if (empty($plugin_info)) {
+      $plugin_info = array(
+        'name' => $plugin['name'],
+        'url' => "http://dev.bukkit.org/server-mods/".$plugin['name'],
+        'last_update' => time(),
+        'versions' => array(),
+      );
+    }
+    elseif (!empty($plugin_info['versions'])) {
+      $added_versions = array();
+      foreach ($plugin_info['versions'] as $version) {
+        $added_versions[] = $version['file_name'];
+      }
+    }
+
+    if (!empty($plugin_info['url'])) {
+      echo "Updating Version History of ".$plugin['name']."...";
+      $html = file_get_contents($plugin_info['url']);
+
+      if (preg_match_all("/<dt>Recent files<\/dt>\s*<dd><ul>(.*)<\/ul><\/dd>/m", $html, $matches)) {
+        $count = preg_match_all('/<li><span.*?>(.*?)<\/span>.*?<a href="(.*?)">(.*?)<\/a> for (.*?)<span.*?data-epoch="(.*?)".*?>(.*?)<\/span><\/li>/m', $matches[1][0], $available_files, PREG_SET_ORDER);
+
+        $new_versions = array();
+        foreach($available_files as $files) {
+          $version = array(
+            'type' => trim($files[1]), //R, B, A
+            'url'  => 'http://dev.bukkit.org'.trim($files[2]),
+            'file_name' => trim($files[3]),
+            'bukkit_version' => trim($files[4]),
+            'time' => trim($files[5]),
+            'time_string' => trim($files[6]),
+          );
+          if (isset($added_versions) && !in_array($version['file_name'], $added_versions) || !isset($added_versions)) {
+            array_unshift($new_versions, $version);
+          }
+        }
+        $plugin_info['versions'] += $new_versions;
+      }
+      else {
+        unset($plugin_info['url']);
+      }
+      $server_mods->save($plugin_info);
+      echo "done\n";
+    }
+
+  }
+
+  $current_version = NULL;
+  if (preg_match('/(.*?) \[(.*?)\]/', $plugin['version'], $match)) {
+    $plugin_version = $match[1];
+    $plugin_bukkit_version = $match[2];
+  }
+  else {
+    $plugin_version = $plugin['version'];
+    $plugin_bukkit_version = NULL;
+  }
+
+  $current_version = getCurrentVersion($plugin_version, $plugin_info['versions']);
+  if (empty($current_version) && preg_match('/(\d+\.\d+(\.\d+)?)/', $plugin_version, $match)) {
+    $plugin_version = $match[1];
+    $current_version = getCurrentVersion($plugin_version, $plugin_info['versions']);
+  }
+
+  if (empty($plugin_info['versions'])) {
+    echo "No version infos for ".$plugin['name']."\n";
+    echo "\tCurrent Version ".$plugin['version']."\n";
+  }
+  elseif (empty($current_version)) {
+    echo "No current version found for ".$plugin['name']."\n";
+    echo "\tCurrent Version ".$plugin['version']."\n";
+    if ($last_version = getLastVersion($rules['software'], $plugin_info['versions']))
+      echo "\tLatest version ".$last_version['file_name']." from ".$last_version['time_string']."\n";
+  }
+  else {
+    $last_version = getLastVersion($rules['software'], $plugin_info['versions']);
+
+    if ($last_version && $last_version['file_name'] != $current_version['file_name'] && $current_version['time_string'] != $last_version['time_string']) {
+      echo "Update for ".$plugin['name']."\n";
+      echo "\tCurrent Version ".$plugin['version']." from ".$current_version['time_string']."\n";
+      echo "\tLatest Version ".$last_version['file_name']." from ".$last_version['time_string']."\n";
+    }
+    else {
+      $last_version = array_pop($plugin_info['versions']);
+      if ($last_version['file_name'] != $current_version['file_name'] && $current_version['time_string'] != $last_version['time_string']) {
+        echo "Non-Equal CB Version Update for ".$plugin['name']."\n";
+        echo "\tCurrent Version ".$plugin['version']." from ".$current_version['time_string']."\n";
+        echo "\tLatest Version ".$last_version['file_name']." from ".$last_version['time_string']." for ".$last_version['bukkit_version']."\n";
+      }
+    }
+  }
+}

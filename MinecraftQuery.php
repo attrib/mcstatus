f558775ebbf1e2e6507cc51a31ca12412c7f55d1
kfritsche <karl.fritsche@cocomore.com>
2012-08-17 21:32:46 +0200

    Init

---
+++ MinecraftQuery.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kfritsche
+ * Date: 8/17/12
+ * Time: 3:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class MinecraftQuery {
+
+}
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kfritsche
+ * Date: 8/17/12
+ * Time: 4:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
